c11b15d0e75b4267117065cd7a2d8bcc2f52224b
pritishraj04 <pritishraj04@gmail.com>
2022-02-22 16:44:04 +0530

    prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import "dotenv/config";
 import goalsRouter from "./routes/goalsRoutes.js";
 import usersRouter from "./routes/userRoutes.js";
 import { errorHandler } from "./middlewares/errorMiddleware.js";
 import connectDB from "./config/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 connectDB();
 
@@ -15,6 +20,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalsRouter);
 app.use("/api/users", usersRouter);
+
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("*", (req, res) => res.send("Please set to Production"));
+}
 
 app.use(errorHandler);
 
